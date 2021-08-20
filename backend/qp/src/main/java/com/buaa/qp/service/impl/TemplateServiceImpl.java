4172e80c48f159f7186ad8ec87ce842d983359d9
YQ-zhishen <yangqian_1015@icloud.com>
2021-08-21 00:26:10 +0800

    Finished development of interface "/normal/submit"

--- backend/qp/src/main/java/com/buaa/qp/service/impl/TemplateServiceImpl.java
+++ backend/qp/src/main/java/com/buaa/qp/service/impl/TemplateServiceImpl.java
@@ -1,21 +1,40 @@
 package com.buaa.qp.service.impl;
 
+import com.buaa.qp.dao.QuestionDao;
+import com.buaa.qp.dao.TemplateDao;
 import com.buaa.qp.entity.Question;
 import com.buaa.qp.entity.Template;
 import com.buaa.qp.service.TemplateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
 @Service
 public class TemplateServiceImpl implements TemplateService {
+    @Autowired
+    private TemplateDao templateDao;
+
+    @Autowired
+    private QuestionDao questionDao;
+
     @Override
     public void submitTemplate(Template template, ArrayList<Question> questions) {
-
+        Integer templateId = templateDao.insert(template);
+        for (Question question : questions) {
+            question.setTemplateId(templateId);
+            questionDao.insert(question);
+        }
     }
 
     @Override
     public void modifyTemplate(Template template, ArrayList<Question> questions) {
-
+        templateDao.update(template);
+        Integer templateId = template.getTemplateId();
+        questionDao.deleteByTid(templateId);
+        for (Question question : questions) {
+            question.setTemplateId(templateId);
+            questionDao.insert(question);
+        }
     }
 }
