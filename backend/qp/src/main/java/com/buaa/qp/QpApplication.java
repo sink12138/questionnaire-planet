58d3a1d90dcf2f2f12250a3b8d21eb40361e69e3
YQ-zhishen <yangqian_1015@icloud.com>
2021-08-20 19:26:30 +0800

    Created project

---
+++ backend/qp/src/main/java/com/buaa/qp/QpApplication.java
@@ -0,0 +1,13 @@
+package com.buaa.qp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QpApplication.class, args);
+	}
+
+}
