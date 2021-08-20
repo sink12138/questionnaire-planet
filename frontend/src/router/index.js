933f881cef3cfa1a4892dd7cf5fff32bb4e2669d
cdg <271105328@qq.com>
2021-08-21 00:04:39 +0800

    用户注册登录页面第一次提交

---
+++ frontend/src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component:Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../components/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('../components/Register.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <h1>主页</h1>
+    <div id="nav">
+      <router-link to="/"><button class="homepage">问卷星球</button></router-link> |
+      <router-link to="/login"><button class="login">登录</button></router-link> /
+      <router-link to="/register"><button class="register">注册</button></router-link>
+    </div>
+  </div>
+</template>
