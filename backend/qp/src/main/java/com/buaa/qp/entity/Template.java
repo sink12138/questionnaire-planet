67470f10dd4fa9e18e580b318c09b3782745def8
YQ-zhishen <yangqian_1015@icloud.com>
2021-08-21 12:26:29 +0800

    Changed database structure of table "template"; edited template entity and sql scripts (controller yet to edit)

--- backend/qp/src/main/java/com/buaa/qp/entity/Template.java
+++ backend/qp/src/main/java/com/buaa/qp/entity/Template.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.sql.Time;
+import java.util.Date;
 
 @Entity
 @Setter
@@ -16,6 +18,8 @@ public class Template {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer templateId;
 
+    private String type;
+
     private Integer owner;
 
     private String title;
@@ -23,6 +27,16 @@ public class Template {
     private String description;
 
     private String password;
+
+    private Boolean released = false;
+
+    private Boolean deleted = false;
+
+    private Date creationTime;
+
+    private Date releaseTime;
+
+    private Time duration;
 
     public Template() {
 
