2148864058d3b1534ba77afc79bf792cc6164359
qian12138 <244901960@qq.com>
2021-08-21 16:39:35 +0800

    scroll

--- frontend/src/views/Questionnaire.vue
+++ frontend/src/views/Questionnaire.vue
@@ -1,65 +1,107 @@
 <template>
   <div id="Questionnaire">
-      <div class="banner">
-          <div class="type create_by_normal">
-              <div class="inner">
-                    <div class="type_show">
-                        <question-pic></question-pic>
-                    </div>
-                    <h2 class="title">创建普通问卷</h2>
-                    <h3 class="subtitle">创建包含单选、多选等题型的问卷</h3>
-                    <router-link to="/normal/new">
-                        <el-button>开始</el-button>
-                    </router-link>
-              </div>
-          </div>
-          <div class="type create_by_vote">
-              <div class="inner">
-                    <div class="type_show">
-                        <question-pic></question-pic>
-                    </div>
-                    <h2 class="title">创建投票问卷</h2>
-                    <h3 class="subtitle">创建包含投票功能的问卷</h3>
-                    <router-link to="/vote/new">
-                        <el-button>开始</el-button>
-                    </router-link>
-              </div>
-          </div>
-          <div class="type create_by_apply">
-              <div class="inner">
-                    <div class="type_show">
-                        <question-pic></question-pic>
-                    </div>
-                    <h2 class="title">创建报名问卷</h2>
-                    <h3 class="subtitle">创建限制选项选择人数的问卷</h3>
-                    <router-link to="/apply/new">
-                        <el-button>开始</el-button>
-                    </router-link>
-              </div>
-          </div>
-          <div class="type create_by_exam">
-              <div class="inner">
-                    <div class="type_show">
-                        <question-pic></question-pic>
-                    </div>
-                    <h2 class="title">创建考试问卷</h2>
-                    <h3 class="subtitle">创建规定时间自动提交的问卷</h3>
-                    <router-link to="/exam/new">
-                        <el-button>开始</el-button>
-                    </router-link>
-              </div>
-          </div>
-      </div>
+        <div class="banner">
+            <div class="type create_by_normal">
+                <div class="inner">
+                        <div class="type_show">
+                            <question-pic></question-pic>
+                        </div>
+                        <h2 class="title">创建普通问卷</h2>
+                        <h3 class="subtitle">创建包含单选、多选等题型的问卷</h3>
+                        <router-link to="/normal/new">
+                            <el-button>开始</el-button>
+                        </router-link>
+                </div>
+            </div>
+            <div class="type create_by_vote">
+                <div class="inner">
+                        <div class="type_show">
+                            <question-pic></question-pic>
+                        </div>
+                        <h2 class="title">创建投票问卷</h2>
+                        <h3 class="subtitle">创建包含投票功能的问卷</h3>
+                        <router-link to="/vote/new">
+                            <el-button>开始</el-button>
+                        </router-link>
+                </div>
+            </div>
+            <div class="type create_by_apply">
+                <div class="inner">
+                        <div class="type_show">
+                            <question-pic></question-pic>
+                        </div>
+                        <h2 class="title">创建报名问卷</h2>
+                        <h3 class="subtitle">创建限制选项选择人数的问卷</h3>
+                        <router-link to="/apply/new">
+                            <el-button>开始</el-button>
+                        </router-link>
+                </div>
+            </div>
+            <div class="type create_by_exam">
+                <div class="inner">
+                        <div class="type_show">
+                            <question-pic></question-pic>
+                        </div>
+                        <h2 class="title">创建考试问卷</h2>
+                        <h3 class="subtitle">创建规定时间自动提交的问卷</h3>
+                        <router-link to="/exam/new">
+                            <el-button>开始</el-button>
+                        </router-link>
+                </div>
+            </div>
+        </div>
+        <vue-seamless-scroll :data="listData" class="warp">
+            <ul class="item">
+            <li v-for="(item, index) in listData" :key="index">
+                <span class="title" v-text="item.title"></span>
+                <span class="date" v-text="item.date"></span>
+            </li>
+            </ul>
+        </vue-seamless-scroll>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 import svg from "../components/svg-questionnaire.vue"
+import vueSeamlessScroll from 'vue-seamless-scroll'
 export default {
     components: {
-        'question-pic': svg
-    }
+        'question-pic': svg,
+        vueSeamlessScroll
+    },
+    data () {
+      return {
+        listData: [{
+          'title': '无缝滚动第一行无缝滚动第一行',
+          'date': '2017-12-16'
+        }, {
+          'title': '无缝滚动第二行无缝滚动第二行',
+          'date': '2017-12-16'
+        }, {
+          'title': '无缝滚动第三行无缝滚动第三行',
+          'date': '2017-12-16'
+        }, {
+          'title': '无缝滚动第四行无缝滚动第四行',
+          'date': '2017-12-16'
+        }, {
+          'title': '无缝滚动第五行无缝滚动第五行',
+          'date': '2017-12-16'
+        }, {
+          'title': '无缝滚动第六行无缝滚动第六行',
+          'date': '2017-12-16'
+        }, {
+          'title': '无缝滚动第七行无缝滚动第七行',
+          'date': '2017-12-16'
+        }, {
+          'title': '无缝滚动第八行无缝滚动第八行',
+          'date': '2017-12-16'
+        }, {
+          'title': '无缝滚动第九行无缝滚动第九行',
+          'date': '2017-12-16'
+        }],
+      }
+    },
 }
 </script>
 
@@ -116,5 +158,28 @@ export default {
 .create_by_exam,
 .create_by_exam .el-button {
     background-color: rgba(46, 219, 161, 0.94);
+}
+</style>
+
+<style lang="scss" scoped>
+.warp {
+height: 270px;
+width: 360px;
+margin: 0 auto;
+overflow: hidden;
+ul {
+    list-style: none;
+    padding: 0;
+    margin: 0 auto;
+    li,
+    a {
+    display: block;
+    height: 30px;
+    line-height: 30px;
+    display: flex;
+    justify-content: space-between;
+    font-size: 15px;
+    }
+}
 }
 </style>
