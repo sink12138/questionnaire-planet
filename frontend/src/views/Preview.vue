e37ae31dcab4835d59585d5fd500f009a101f664
qian12138 <244901960@qq.com>
2021-08-22 16:54:39 +0800

    control width

--- frontend/src/views/Preview.vue
+++ frontend/src/views/Preview.vue
@@ -10,55 +10,55 @@
         </h3>
       </div>
       <div class="question">
-        <ul id="quests">
-          <li v-for="(item, index_question) in questions" :key="index_question">
-            <div class="question-title">
-              <h3>第{{ index_question + 1 }}题,题目:{{ item.stem }}</h3>
-              <a>题目描述：{{ item.description }}</a>
+        <div v-for="(item, index_question) in questions" :key="index_question">
+          <div class="question-title">
+            <div>第{{ index_question + 1 }}题,题目:{{ item.stem }}</div>
+            <a>题目描述：{{ item.description }}</a>
+          </div>
+          <div class="question-content">
+            <div v-if="item.type == 'choice'">
+              <el-radio-group v-model="answers[index_question]" v-for="(i, index) in item.choices" :key="index" @change="changeValue">
+                <el-radio :label="index">{{ i }}</el-radio>
+              </el-radio-group>
             </div>
-            <div class="question-content">
-              <div v-if="item.type == 'choice'">
-                <el-radio-group v-model="answers[index_question]" v-for="(i, index) in item.choices" :key="index" @change="changeValue">
-                  <el-radio :label="index">{{ i }}</el-radio>
-                </el-radio-group>
-              </div>
-              <div v-if="item.type == 'multi-choice'">
-                <el-checkbox-group
-                v-model="multi" 
-                v-for="(i, index) in item.choices"
-                :min = "item.min"
-                :max = "item.max"
-                :key="index" 
-                @change="multiChangeValue(index_question)">
-                  <el-checkbox :label="index" border>{{ i }}</el-checkbox>
-                </el-checkbox-group>
-              </div>
-              <div v-if="item.type == 'filling'">
-                <el-input
-                type="textarea"
-                :rows="2"
-                placeholder="请输入内容"
-                v-model="answers[index_question]">
-                </el-input>
-              </div>
-              <div v-if="item.type == 'grade'">
-                <el-radio-group v-model="answers[index_question]" v-for="(i, index) in item.choices" :key="index" @change="changeValue">
-                  <el-radio :label="index">{{ i }}({{item.scores[ index ]}})</el-radio>
-                </el-radio-group>
-              </div>
-              <div v-if="item.type == 'dropdown'">
-                <el-select v-model="answers[index_question]" clearable placeholder="请选择">
-                  <el-option
-                    v-for="(i, index) in item.choices"
-                    :key="index"
-                    :label="i"
-                    :value="index">
-                  </el-option>
-                </el-select>
-              </div>
+            <div v-if="item.type == 'multi-choice'">
+              <el-checkbox-group
+              v-model="multi" 
+              v-for="(i, index) in item.choices"
+              :min = "item.min"
+              :max = "item.max"
+              :key="index" 
+              @change="multiChangeValue(index_question)">
+                <el-checkbox :label="index" border>{{ i }}</el-checkbox>
+              </el-checkbox-group>
             </div>
-          </li>
-        </ul>
+            <div v-if="item.type == 'filling'">
+              <el-input
+              type="textarea"
+              class="input"
+              :rows="item.height"
+              :style="{'--width': item.width}"
+              placeholder="请输入内容"
+              v-model="answers[index_question]">
+              </el-input>
+            </div>
+            <div v-if="item.type == 'grade'">
+              <el-radio-group v-model="answers[index_question]" v-for="(i, index) in item.choices" :key="index" @change="changeValue">
+                <el-radio :label="index">{{ i }}({{item.scores[ index ]}})</el-radio>
+              </el-radio-group>
+            </div>
+            <div v-if="item.type == 'dropdown'">
+              <el-select v-model="answers[index_question]" clearable placeholder="请选择">
+                <el-option
+                  v-for="(i, index) in item.choices"
+                  :key="index"
+                  :label="i"
+                  :value="index">
+                </el-option>
+              </el-select>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
@@ -94,7 +94,15 @@ export default {
           choices: [
             "hello","question","whats up"
           ],
-          check: ""
+        },
+        {
+          type: "choice",
+          stem: "666",
+          description: "testtesttesttesttest",
+          required: true,
+          choices: [
+            "hello","question","whats up"
+          ],
         },
         {
           type: "multi-choice",
@@ -106,16 +114,14 @@ export default {
           ],
           max: 2,
           min: 0,
-          checkList: []
         },
         {
           type: "filling",
           stem: "3",
           description: "testtesttesttesttest",
           required: true,
-          height: 2,
-          width: 500,
-          textarea: ""
+          height: 3,
+          width: '1000px',
         },
         {
           type: "grade",
@@ -136,8 +142,7 @@ export default {
           required: true,
           choices: [
             "hello","question","whats up"
-          ],
-          check: ""
+          ]
         }
       ],
       multi: [],
@@ -195,3 +200,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.question-content .input {
+  width: var(--width)
+}
+</style>
