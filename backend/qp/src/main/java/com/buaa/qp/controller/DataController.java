2d88a464df2f28ed6c56d36066c5ff050f126717
YQ-zhishen <yangqian_1015@icloud.com>
2021-08-22 22:08:55 +0800

    Finished interface "/excel" for data downloading

--- backend/qp/src/main/java/com/buaa/qp/controller/DataController.java
+++ backend/qp/src/main/java/com/buaa/qp/controller/DataController.java
@@ -5,15 +5,29 @@ import com.alibaba.fastjson.JSONArray;
 import com.buaa.qp.entity.Answer;
 import com.buaa.qp.entity.Question;
 import com.buaa.qp.entity.Template;
+import com.buaa.qp.exception.ExtraMessageException;
 import com.buaa.qp.exception.LoginVerificationException;
 import com.buaa.qp.exception.ObjectNotFoundException;
+import com.buaa.qp.exception.ParameterFormatException;
 import com.buaa.qp.service.AnswerService;
 import com.buaa.qp.service.TemplateService;
+import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.Number;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -29,6 +43,9 @@ public class DataController {
 
     @Autowired
     private HttpServletRequest request;
+
+    @Autowired
+    private HttpServletResponse response;
 
     @GetMapping("/data")
     public Map<String, Object> data(@RequestParam("templateId") Integer templateId) {
@@ -61,6 +78,93 @@ public class DataController {
             map.put("message", "操作失败");
         }
         return map;
+    }
+
+    @GetMapping("/excel")
+    public void excel(@RequestParam(value = "templateId", required = false) String idStr) {
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        File file = new File(System.currentTimeMillis() + ".xls");
+        try {
+            // Parameter checks
+            int templateId;
+            if (idStr == null)
+                throw new ParameterFormatException();
+            try {
+                templateId = Integer.parseInt(idStr);
+            }
+            catch (NumberFormatException nfe) {
+                throw new ParameterFormatException();
+            }
+            if (templateId <= 0)
+                throw new ParameterFormatException();
+
+            // Existence checks
+            Template template = templateService.getTemplate(templateId);
+            if (template == null)
+                throw new ObjectNotFoundException();
+
+            // Authority checks
+            Integer accountId = (Integer) request.getSession().getAttribute("accountId");
+            if (!template.getOwner().equals(accountId))
+                throw new LoginVerificationException();
+            if (template.getDeleted())
+                throw new ExtraMessageException("无法操作已删除的问卷");
+
+            ArrayList<Answer> answers = answerService.getAnswersByTid(templateId);
+            ArrayList<Question> questions = templateService.getQuestionsByTid(templateId);
+            ArrayList<ArrayList<String>> formContent = getData(answers, questions);
+            if (file.exists() && !file.delete())
+                throw new ExtraMessageException("File already exists");
+            if(!file.createNewFile())
+                throw new ExtraMessageException("File already exists");
+            WritableWorkbook workbook = Workbook.createWorkbook(file);
+            WritableSheet sheet = workbook.createSheet("sheet1", 0);
+            for (int i = 0; i < formContent.get(0).size(); ++i) {
+                sheet.addCell(new Label(i, 0, formContent.get(0).get(i)));
+            }
+            for (int i = 1; i < formContent.size(); ++i) {
+                sheet.addCell(new Number(0, i, i));
+                for (int j = 1; j < formContent.get(i).size(); ++j) {
+                    sheet.addCell(new Label(j, i, formContent.get(i).get(j)));
+                }
+            }
+            workbook.write();
+            workbook.close();
+            response.setContentType("application/force-download");
+            response.addHeader(
+                    "Content-Disposition",
+                    "attachment; filename=" + URLEncoder.encode(
+                            template.getTitle() + "_数据汇总.xls", "UTF-8"
+                    ).replaceAll("\\+", "%20")
+            );
+            byte[] buffer = new byte[1024];
+            fis = new FileInputStream(file);
+            bis = new BufferedInputStream(fis);
+            OutputStream os = response.getOutputStream();
+            int i = bis.read(buffer);
+            while (i != -1) {
+                os.write(buffer, 0, i);
+                i = bis.read(buffer);
+            }
+        }
+        catch (LoginVerificationException | ParameterFormatException |
+                ObjectNotFoundException | ExtraMessageException exc) {
+            System.out.println(exc);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (bis != null) bis.close();
+                if (fis != null) fis.close();
+                if (!file.delete()) System.out.println("Failed to delete file");
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private ArrayList<ArrayList<String>> getData(ArrayList<Answer> answers, ArrayList<Question> questions) {
