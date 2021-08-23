e562f55aaf5e34d95aa264b7964ec61d21361b15
qian12138 <244901960@qq.com>
2021-08-23 21:19:29 +0800

    statistics ui

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,12 +9,13 @@
 </script>
 
 <style>
+html,body,#app{
+  height: 100%;
+  margin: 0;
+}
 #app {
   font-family: 微软雅黑;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 textarea {
 	font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
